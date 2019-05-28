ceb085eee875b7e4641577aa729d80fa13c68381
Matthew Ryan Hagarty <matthewryanhagarty@gmail.com>
2019-05-28 16:04:02 -0700

    created randomly generated numbers for totalDollarAmount & totalDollarsSpent. console logged to ensure both numbers were within their respective range.

---
+++ assets/javascript/game.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+
+var wins = 0;
+var losses =0;
+
+var minAmountNumber = 19;
+var maxAmountNumber = 120;
+var minSpentNumber = 1;
+var maxSpentNumber = 12;
+
+var totalDollarAmount = Math.floor((Math.random() * (+maxAmountNumber - +minAmountNumber) + +minAmountNumber));
+
+var totalDollarsSpent = Math.floor((Math.random() * (+maxSpentNumber - +minSpentNumber) + +minSpentNumber));
+
+// console.log(totalDollarAmount);
+console.log(totalDollarsSpent);
+
+
+
+
+
+// // randomly generated number to start the game ^
+
+// var totalDollarsSpent = "";
+
+// // this number will increase based on the user's click selections. it will be the sum of all the clicks ^
+
+
+
+
+
+
+
+});
