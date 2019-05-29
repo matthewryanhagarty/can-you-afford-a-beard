cd68c9177363e3d218a9dd914893e258f1eb5d5f
Matthew Ryan Hagarty <matthewryanhagarty@gmail.com>
2019-05-28 21:33:25 -0700

    added if/else statements, but they don't appear to be working correctly. this is probably due to where i have positioned certain things. my reset function also isn't triggering correctly. going to rearrange where things are placed and defined to see if i can work out the bugs.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,6 +3,9 @@ $(document).ready(function(){
 var wins = 0;
 var losses =0;
 
+$("#wins").text(wins);
+$("#losses").text(losses);
+
 var minAmountNumber = 19;
 var maxAmountNumber = 120;
 var minSpentNumber = 1;
@@ -16,29 +19,45 @@ var totalDollarsSpent = 0;
 
 $("#total-dollars-spent").text(totalDollarsSpent);
 
+
+function reset() {
+  totalDollarAmount = "";
+  totalDollarsSpent = 0;
+};
+
+
+
 var imageNumber1= 1;
 var imageNumber2= Math.floor((Math.random() * (+maxSpentNumber - +minSpentNumber) + +minSpentNumber));
 var imageNumber3= Math.floor((Math.random() * (+maxSpentNumber - +minSpentNumber) + +minSpentNumber));
 var imageNumber4= Math.floor((Math.random() * (+maxSpentNumber - +minSpentNumber) + +minSpentNumber));
 
-// console.log(totalDollarAmount);
-// console.log(totalDollarsSpent);
+
 
 var oil = imageNumber1;
 var shampoo = imageNumber2;
 var conditioner = imageNumber3;
 var comb = imageNumber4;
 
-// console.log(oil);
-// console.log(shampoo);
-// console.log(conditioner);
-// console.log(comb);
+
 
 $("#oil").on("click", function() {
   var imageValue = oil;
   imageValue = parseInt(imageValue);
   totalDollarsSpent += imageValue;
   $("#total-dollars-spent").text(totalDollarsSpent);
+
+  if (totalDollarsSpent === totalDollarAmount) {
+    wins++;
+    $("#wins").text(wins);
+    reset();
+  } else if
+    (totalDollarsSpent >= totalDollarAmount) {
+      alert("DUDE, save some of that cash money for rent.");
+      losses++;
+      $("#losses").text(losses);
+      reset();
+  }
 });
 
 $("#shampoo").on("click", function() {
@@ -46,13 +65,38 @@ $("#shampoo").on("click", function() {
   imageValue = parseInt(imageValue);
   totalDollarsSpent += imageValue;
   $("#total-dollars-spent").text(totalDollarsSpent);
+
+  if (totalDollarsSpent === totalDollarAmount) {
+    wins++;
+    $("#wins").text(wins);
+    reset();
+  } else if
+    (totalDollarsSpent >= totalDollarAmount) {
+      alert("DUDE, save some of that cash money for rent.");
+      losses++;
+      $("#losses").text(losses);
+      reset();
+  }
 });
 
 $("#conditioner").on("click", function() {
+  
   var imageValue = conditioner;
   imageValue = parseInt(imageValue);
   totalDollarsSpent += imageValue;
   $("#total-dollars-spent").text(totalDollarsSpent);
+
+  if (totalDollarsSpent === totalDollarAmount) {
+    wins++;
+    $("#wins").text(wins);
+    reset();
+  } else if
+    (totalDollarsSpent >= totalDollarAmount) {
+      alert("DUDE, save some of that cash money for rent.");
+      losses++;
+      $("#losses").text(losses);
+      reset();
+  }
 });
 
 $("#comb").on("click", function() {
@@ -60,21 +104,19 @@ $("#comb").on("click", function() {
   imageValue = parseInt(imageValue);
   totalDollarsSpent += imageValue;
   $("#total-dollars-spent").text(totalDollarsSpent);
+
+  if (totalDollarsSpent === totalDollarAmount) {
+    wins++;
+    $("#wins").text(wins);
+    reset();
+  } else if
+    (totalDollarsSpent >= totalDollarAmount) {
+      alert("DUDE, save some of that cash money for rent.");
+      losses++;
+      $("#losses").text(losses);
+      reset();
+  }
 });
-
-
-
-
-
-
-// // randomly generated number to start the game ^
-
-// var totalDollarsSpent = "";
-
-// // this number will increase based on the user's click selections. it will be the sum of all the clicks ^
-
-
-
 
 
 
